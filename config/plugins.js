5c181bbbd0ca539833a28e685dee8ea857a90033
Luiz Otávio <luizomf@gmail.com>
2022-06-01 19:18:33 -0300

    Update database config

--- config/plugins.js
+++ config/plugins.js
@@ -7,6 +7,7 @@ module.exports = ({ env }) => ({
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
       },
+
       actionOptions: {
         upload: {},
         delete: {},
